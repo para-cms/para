82686c4dad36de82384f279cb9964031f291a21b
zenati <zenati@informatique.univ-paris-diderot.fr>
2014-09-24 15:05:45 +0200

    Generates model with belongs_to

--- lib/generators/para/resource/resource_generator.rb
+++ lib/generators/para/resource/resource_generator.rb
@@ -20,5 +20,15 @@ module Para
         "\n      resources :#{plural_file_name}"
       end
     end
+
+    def generate_model
+      generate 'model', file_name
+    end
+
+    def insert_belongs_to_to_model
+      inject_into_file "app/models/#{file_name}.rb", after: "class #{component_name.camelize} < ActiveRecord::Base" do
+        "\n  belongs_to :component, class_name: 'Para::Component::Base'"
+      end
+    end
   end
 end
