2a862db73c830827993f2f3b0c52e7517f2cef9d
zenati <zenati@informatique.univ-paris-diderot.fr>
2014-09-30 10:42:02 +0200

    We now use config.pagination_theme to specify the pagination theme

--- lib/para/config.rb
+++ lib/para/config.rb
@@ -5,5 +5,8 @@ module Para
 
     mattr_accessor :current_admin_method
     @@current_admin_method = :current_admin_user
+
+    mattr_accessor :pagination_theme
+    @@pagination_theme = :pagination_theme
   end
 end
