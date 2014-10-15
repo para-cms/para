9d70a3002d786bd2402508c723092c9e0fd54c18
zenati <zenati@users.noreply.github.com>
2014-10-15 14:31:45 +0200

    Update model_helper.rb

--- app/helpers/para/model_helper.rb
+++ app/helpers/para/model_helper.rb
@@ -18,8 +18,7 @@ module Para
     end
 
     def field_value_for(model, field_name)
-      fields = model_field_mappings(model.class).fields_hash
-      fields[field_name.to_sym].value_for(model)
+      model_field_mappings(model.class).fields_hash[field_name.to_sym].value_for(model)
     end
   end
 end
