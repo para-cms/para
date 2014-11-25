239f466af9a9811c3583bc7ae04140c0d81a2347
vala <gonjo@free.fr>
2014-11-25 10:52:57 +0100

    add component routes to the right :admin namespace, fixes #3

--- lib/generators/para/component/component_generator.rb
+++ lib/generators/para/component/component_generator.rb
@@ -29,11 +29,11 @@ module Para
     def add_route
       route_file = File.read(Rails.root.join('config/routes.rb'))
 
-      unless route_file.match /namespace :admin do/
+      unless route_file.match /^\s+namespace :admin do/
         route "namespace :admin do\n  end\n"
       end
 
-      inject_into_file 'config/routes.rb', after: 'namespace :admin do' do
+      inject_into_file 'config/routes.rb', after: '  namespace :admin do' do
         "\n    component :#{ file_name } do\n    end"
       end
     end
