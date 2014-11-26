87a91347bc623d72cf99139b9372b2e54a63dc99
vala <gonjo@free.fr>
2014-11-26 16:56:13 +0100

    refactor alerts and flashes to be used in a more flexible fashion

--- app/helpers/para/markup_helper.rb
+++ app/helpers/para/markup_helper.rb
@@ -1,5 +1,9 @@
 module Para
   module MarkupHelper
+    def alert(message, options = {}, &block)
+      Para::Markup::Alert.new(self).container(message, options, &block)
+    end
+
     def resources_table(options = {}, &block)
       Para::Markup::ResourcesTable.new(self).container(options, &block)
     end
