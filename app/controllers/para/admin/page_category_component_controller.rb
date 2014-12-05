7dc99cdf03432ef795eaca5b6e8ac3ea01269916
vala <gonjo@free.fr>
2014-12-05 16:55:17 +0100

    simplify README in favor of Wiki for now

--- app/controllers/para/admin/page_category_component_controller.rb
+++ app/controllers/para/admin/page_category_component_controller.rb
@@ -3,9 +3,13 @@ require_dependency "para/application_controller"
 module Para
   module Admin
     class PageCategoryComponentController < Para::Admin::ComponentController
+      load_and_authorize_resource :page, class: 'Para::Page',
+                                         parent: false,
+                                         through: :component
+
       def show
-        @q = @component.pages.search(params[:q])
-        @pages = @q.result.page(params[:page]).per(10)
+        @q = @pages.search(params[:q])
+        @pages = @q.result.page(params[:page])
       end
     end
   end
