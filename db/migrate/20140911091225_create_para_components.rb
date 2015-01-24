b1eebdda55fa1fe78c2315a62341373cced99024
vala <gonjo@free.fr>
2015-01-24 13:50:21 +0100

    make para/component/base#configuration default to empty hash work

--- db/migrate/20140911091225_create_para_components.rb
+++ db/migrate/20140911091225_create_para_components.rb
@@ -5,7 +5,7 @@ class CreateParaComponents < ActiveRecord::Migration
     create_table :para_components do |t|
       t.string :type
       t.string :name
-      t.hstore :configuration, default: {}
+      t.hstore :configuration, default: '', null: false
       t.integer :position, default: 0
       t.integer :component_section_id
 
