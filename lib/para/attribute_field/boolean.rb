617b897264ea3c325d052bfdaacbefc9d1bc0d6c
vala <gonjo@free.fr>
2015-01-26 10:03:28 +0100

    add file_field, make it the default for attachments, and default to image_field only if attachment has styles and the first style's value looks like an image size

--- lib/para/attribute_field/boolean.rb
+++ lib/para/attribute_field/boolean.rb
@@ -7,8 +7,6 @@ module Para
         I18n.t("para.types.boolean.#{ (!!instance.send(name)).to_s }")
       end
 
-      private
-      
       def wrapper_name
         :horizontal_radio_and_checkboxes
       end
--- lib/para/model_field_parsers/paperclip.rb
+++ lib/para/model_field_parsers/paperclip.rb
@@ -4,15 +4,21 @@ module Para
       register :paperclip, self
 
       def parse!
-        model.attachment_definitions.each do |key, _|
+        model.attachment_definitions.each do |key, options|
           paperclip_suffixes.each do |suffix|
             field_name = [key, suffix].join('_').to_sym
             @fields_hash.delete(field_name)
           end
 
-          @fields_hash[key] = AttributeField::ImageField.new(
-            model, name: key, type: 'image', field_type: 'image'
-          )
+          @fields_hash[key] = if image?(options)
+            AttributeField::ImageField.new(
+              model, name: key, type: 'image', field_type: 'image'
+            )
+          else
+            AttributeField::FileField.new(
+              model, name: key, type: 'file', field_type: 'file'
+            )
+          end
         end
       end
 
@@ -29,6 +35,16 @@ module Para
           :file_size,
           :updated_at
         ]
+      end
+
+      # For our default, we tell that an attachment is an image if it
+      # has at least one style and that this style starts with a digit or the
+      # "x" letter, which is used for styles like "x200" to only force height
+      #
+      def image?(options)
+        if (styles = options[:styles]) && !styles.empty?
+          styles.values.first.match(/^(\d|x)/i)
+        end
       end
     end
   end
