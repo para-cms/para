e8d1b5f45911c1b60a8d67eaef5a3c73575702bd
vala <gonjo@free.fr>
2015-11-28 16:59:06 +0100

    move attribute fields requiring into a standalone attribute_field.rb file

---
+++ lib/para/attribute_field.rb
@@ -0,0 +1,19 @@
+module Para
+  module AttributeField
+  end
+end
+
+require 'para/attribute_field/base'
+require 'para/attribute_field/boolean'
+require 'para/attribute_field/datetime'
+require 'para/attribute_field/password'
+require 'para/attribute_field/file'
+require 'para/attribute_field/image'
+require 'para/attribute_field/enum'
+require 'para/attribute_field/relation'
+require 'para/attribute_field/belongs_to'
+require 'para/attribute_field/has_many'
+require 'para/attribute_field/nested_one'
+require 'para/attribute_field/nested_many'
+require 'para/attribute_field/redactor'
+require 'para/attribute_field/translation'
--- lib/para/attribute_field_mappings.rb
+++ lib/para/attribute_field_mappings.rb
@@ -1,18 +1,3 @@
-require 'para/attribute_field/base'
-require 'para/attribute_field/boolean'
-require 'para/attribute_field/datetime'
-require 'para/attribute_field/password'
-require 'para/attribute_field/file'
-require 'para/attribute_field/image'
-require 'para/attribute_field/enum'
-require 'para/attribute_field/relation'
-require 'para/attribute_field/belongs_to'
-require 'para/attribute_field/has_many'
-require 'para/attribute_field/nested_one'
-require 'para/attribute_field/nested_many'
-require 'para/attribute_field/redactor'
-require 'para/attribute_field/translation'
-
 module Para
   class AttributeFieldMappings
     UNEDITABLE_ATTRIBUTES = %w(id component_id created_at updated_at type)
