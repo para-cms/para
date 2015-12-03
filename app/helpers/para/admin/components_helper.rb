7a2d0dedaf1ab2a5d0e95ed1246683be4a30a24c
vala <gonjo@free.fr>
2015-12-03 03:17:14 +0100

    dashboard with basic components rendering

--- app/helpers/para/admin/components_helper.rb
+++ app/helpers/para/admin/components_helper.rb
@@ -1,4 +1,11 @@
 module Para
   module Admin::ComponentsHelper
+    def ordered_components
+      @component_sections.each_with_object([]) do |section, components|
+        section.components.each do |component|
+          components << component
+        end
+      end.sort_by(&:updated_at)
+    end
   end
 end
