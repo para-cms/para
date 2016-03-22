676eb1e0846eebd5770cf8229ba83330fa4ba217
vala <gonjo@free.fr>
2016-03-22 12:16:10 +0100

    only show accessible components in dashboard, order them by name ASC

--- app/helpers/para/admin/components_helper.rb
+++ app/helpers/para/admin/components_helper.rb
@@ -3,9 +3,9 @@ module Para
     def ordered_components
       @component_sections.each_with_object([]) do |section, components|
         section.components.each do |component|
-          components << component
+          components << component if can?(:read, component)
         end
-      end.sort_by(&:updated_at)
+      end.sort_by(&:name)
     end
   end
 end
