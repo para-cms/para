c786315a7de4a2522e2b2efe79342eaf80b6441b
vala <gonjo@free.fr>
2016-04-07 17:12:07 +0200

    use wrapper: :horizontal_form in datetime fields

--- lib/para/attribute_field/datetime.rb
+++ lib/para/attribute_field/datetime.rb
@@ -3,8 +3,14 @@ module Para
     class DatetimeField < AttributeField::Base
       register :datetime, :date, self
 
+      field_option :wrapper, :wrapper_name
+
       def value_for(instance)
         (value = instance.send(name)) && I18n.l(value)
+      end
+
+      def wrapper_name
+        :horizontal_form
       end
     end
   end
