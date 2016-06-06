387362427622e8ef89c48b5d3eaa5847e870bd10
vala <gonjo@free.fr>
2016-06-06 18:25:36 +0200

    fix singleton resource decorator issue when calling :show action in #relation_path

--- app/decorators/para/component/singleton_resource_decorator.rb
+++ app/decorators/para/component/singleton_resource_decorator.rb
@@ -8,9 +8,19 @@ module Para
       end
 
       def relation_path(controller_or_resource, *nested_resources, **options)
-        nested_resources << :resource if nested_resources.empty?
+        nested = nested_resources.any?
+        nested_resources << :resource unless nested
+        options[:action] = action_option_for(options, nested: nested)
         data = [:admin, self, *nested_resources]
         polymorphic_path(data, options)
+      end
+
+      def action_option_for(options, nested: false)
+        if !nested && options[:action].try(:to_sym) == :show
+          nil
+        else
+          options[:action]
+        end
       end
     end
   end
