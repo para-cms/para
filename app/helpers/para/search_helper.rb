0a128f4d82a814588cad534d5a076d09c169fd2a
vala <gonjo@free.fr>
2016-06-10 15:42:42 +0200

    fix associated model sorting in crud#index

--- app/helpers/para/search_helper.rb
+++ app/helpers/para/search_helper.rb
@@ -18,5 +18,33 @@ module Para
         attribute.attribute_column_path.join('_')
       end.join('_or_')
     end
+
+    # This method allows to unify search results and avoid duplicates, avoiding
+    # SQL DISTINCT errors by adding ORDER BY fields to the DISTINCT selection
+    # automatically
+    #
+    # This fixes a previous issue when trying to order search results with
+    # Ransack sorting feature, when implying associated models
+    #
+    def distinct_search_results(results, search)
+      primary_key = [search.klass.table_name, '*'].join('.')
+
+      sorts = if search
+        search.sorts.each_with_index.map do |sort, index|
+          key = ['sort', index].join('_')
+
+          [
+            [sort.attr.relation.name, sort.attr.name].join('.'),
+            key
+          ].join(' AS ')
+        end
+      else
+        []
+      end
+
+      selects = ([primary_key] + sorts).join(', ')
+
+      results.select(selects).uniq
+    end
   end
 end
