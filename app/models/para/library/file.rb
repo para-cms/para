eb14fa043198e149627df5ed11c46447cdfbe999
vala <gonjo@free.fr>
2016-09-16 11:35:05 +0200

    add Para::Library::File#path to abstract filesystem path and s3 url for consumers

--- app/models/para/library/file.rb
+++ app/models/para/library/file.rb
@@ -5,6 +5,19 @@ module Para
 
       validates :attachment, presence: true
       do_not_validate_attachment_file_type :attachment
+
+      # Return attachment.path or attachment.url depending on the storage
+      # backend, allowing 'openuri' and 'roo' libraries to load easily the
+      # file at the right path, on filesystem or othe storage systems, like S3.
+      #
+      def attachment_path
+        return unless attachment?
+
+        case attachment.options[:storage]
+        when :filesystem then attachment.path
+        else attachment.url
+        end
+      end
     end
   end
 end
