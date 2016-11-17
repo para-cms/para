71cfce3ae6ed2c8f30235d518ab02131df5c7906
vala <gonjo@free.fr>
2016-11-17 15:54:36 +0100

    allow including stylesheets from plugin gems

--- lib/para/plugins/set.rb
+++ lib/para/plugins/set.rb
@@ -13,16 +13,21 @@ module Para
 
       def javascript_includes
         each_with_object([]) do |plugin, collection|
-          collection.concat(javascript_includes_for(plugin))
+          collection.concat(includes_for(:javascript, plugin))
+        end
+      end
+
+      def stylesheet_includes
+        each_with_object([]) do |plugin, collection|
+          collection.concat(includes_for(:stylesheet, plugin))
         end
       end
 
       private
 
-      def javascript_includes_for(plugin)
+      def includes_for(type, plugin)
         mod = Para::Plugins.module_name_for(plugin).constantize
-
-        mod.try(:config).try(:javascript_includes) || []
+        mod.try(:config).try(:"#{ type }_includes") || []
       end
     end
   end
