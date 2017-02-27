6b8336cec0e02a2554960073d6e4b8ba0569adc8
xana68 <alex@glyph.fr>
2017-02-27 17:28:16 +0100

    design filter

--- lib/para/simple_form_config.rb
+++ lib/para/simple_form_config.rb
@@ -5,11 +5,11 @@ module Para
       {
         check_boxes: :para_vertical_radio_and_checkboxes,
         radio_buttons: :para_vertical_radio_and_checkboxes,
-        file: :para_vertical_file_input,
-        boolean: :para_vertical_boolean,
+        boolean: :para_vertical_radio_and_checkboxes,
         datetime: :para_multi_select,
         date: :para_multi_select,
-        time: :para_multi_select
+        time: :para_multi_select,
+        file: :para_vertical_file_input
       }
     end
 
@@ -65,80 +65,6 @@ module Para
           b.optional :readonly
           b.use :label, class: 'control-label'
           b.use :input
-          b.use :error, wrap_with: { tag: 'span', class: 'help-block' }
-          b.use :hint,  wrap_with: { tag: 'p', class: 'help-block' }
-        end
-
-        config.wrappers :para_horizontal_form, tag: 'div', class: 'form-group', error_class: 'has-error' do |b|
-          b.use :html5
-          b.use :placeholder
-          b.optional :maxlength
-          b.optional :minlength
-          b.optional :pattern
-          b.optional :min_max
-          b.optional :readonly
-          b.use :label, class: 'col-sm-3 control-label'
-
-          b.wrapper tag: 'div', class: 'col-sm-9' do |ba|
-            ba.use :input, class: 'form-control'
-            ba.use :error, wrap_with: { tag: 'span', class: 'help-block' }
-            ba.use :hint,  wrap_with: { tag: 'p', class: 'help-block' }
-          end
-        end
-
-        config.wrappers :para_horizontal_file_input, tag: 'div', class: 'form-group', error_class: 'has-error' do |b|
-          b.use :html5
-          b.use :placeholder
-          b.optional :maxlength
-          b.optional :minlength
-          b.optional :readonly
-          b.use :label, class: 'col-sm-3 control-label'
-
-          b.wrapper tag: 'div', class: 'col-sm-9' do |ba|
-            ba.use :input
-            ba.use :error, wrap_with: { tag: 'span', class: 'help-block' }
-            ba.use :hint,  wrap_with: { tag: 'p', class: 'help-block' }
-          end
-        end
-
-        config.wrappers :para_horizontal_boolean, tag: 'div', class: 'form-group', error_class: 'has-error' do |b|
-          b.use :html5
-          b.optional :readonly
-
-          b.wrapper tag: 'div', class: 'col-sm-offset-3 col-sm-9' do |wr|
-            wr.wrapper tag: 'div', class: 'checkbox' do |ba|
-              ba.use :label_input
-            end
-
-            wr.use :error, wrap_with: { tag: 'span', class: 'help-block' }
-            wr.use :hint,  wrap_with: { tag: 'p', class: 'help-block' }
-          end
-        end
-
-        config.wrappers :para_horizontal_radio_and_checkboxes, tag: 'div', class: 'form-group', error_class: 'has-error' do |b|
-          b.use :html5
-          b.optional :readonly
-
-          b.use :label, class: 'col-sm-3 control-label'
-
-          b.wrapper tag: 'div', class: 'col-sm-9' do |ba|
-            ba.use :input
-            ba.use :error, wrap_with: { tag: 'span', class: 'help-block' }
-            ba.use :hint,  wrap_with: { tag: 'p', class: 'help-block' }
-          end
-        end
-
-        config.wrappers :para_inline_form, tag: 'div', class: 'form-group', error_class: 'has-error' do |b|
-          b.use :html5
-          b.use :placeholder
-          b.optional :maxlength
-          b.optional :minlength
-          b.optional :pattern
-          b.optional :min_max
-          b.optional :readonly
-          b.use :label, class: 'sr-only'
-
-          b.use :input, class: 'form-control'
           b.use :error, wrap_with: { tag: 'span', class: 'help-block' }
           b.use :hint,  wrap_with: { tag: 'p', class: 'help-block' }
         end
