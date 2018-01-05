62b02283494fac3a2a09efab18a65e0664b4fcb6
vala <gonjo@free.fr>
2018-01-05 10:14:22 +0100

    ensure page model assigns sections page attribute when built from nested form

--- lib/para/page/model.rb
+++ lib/para/page/model.rb
@@ -6,7 +6,8 @@ module Para
       included do
         has_many :sections, -> { ordered }, class_name: '::Para::Page::Section',
                                             as: :page,
-                                            dependent: :destroy
+                                            dependent: :destroy,
+                                            inverse_of: :page
         accepts_nested_attributes_for :sections, allow_destroy: true
       end
     end
