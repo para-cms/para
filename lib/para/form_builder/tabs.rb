7bf33fe36fcf6b14659f41ec20004f3a150fe2e8
vala <gonjo@free.fr>
2018-04-24 18:32:53 +0200

    allow manually setting active tab in form tabs

--- lib/para/form_builder/tabs.rb
+++ lib/para/form_builder/tabs.rb
@@ -4,6 +4,7 @@ module Para
       def tabs(options = {}, &block)
         manager = TabsManager.new(template, object, self, options)
         block.call(manager)
+        manager.finalize!
 
         template.render partial: 'para/form/tabs', locals: { tabs_manager: manager, tabs: manager.tabs }
       end
@@ -19,12 +20,31 @@ module Para
         end
 
         def tab(identifier, options = {}, &block)
-          tabs << Tab.new(template, object, builder, identifier, options, &block)
+          tabs << Tab.new(template, object, builder, identifier, options, tabs.length, &block)
           nil
         end
 
         def tabs
           @tabs ||= []
+        end
+
+        def finalize!
+          # Set all tabs as inactive if one of the tabs was set to be active
+          # manually
+          if tabs.any?(&:active)
+            active_already_set = false
+
+            tabs.each do |tab|
+              # Get a boolean value for the current active state of the tab
+              tab_active = !!tab.active
+
+              tab.active = !active_already_set && tab_active
+
+              # Set the "already set" flag to true once we found one active
+              # tab, forcing `active = false` for all further tabs
+              active_already_set ||= tab_active
+            end
+          end
         end
 
         def affix?
@@ -33,17 +53,22 @@ module Para
       end
 
       class Tab
-        attr_reader :template, :object, :builder, :identifier, :icon, :content
+        attr_reader :template, :object, :builder, :identifier, :icon, :content,
+                    :index
+
+        attr_accessor :active
 
         delegate :capture, to: :template
 
-        def initialize(template, object, builder, identifier, options, &content_block)
+        def initialize(template, object, builder, identifier, options, index, &content_block)
           @template = template
           @object = object
           @builder = builder
           @identifier = identifier
           @content = capture { content_block.call }
           @icon = options[:icon]
+          @active = options[:active]
+          @index = index
         end
 
         def title
@@ -60,6 +85,10 @@ module Para
             builder.nested_resource_dom_id.presence,
             identifier.to_s.parameterize
           ].compact.join('-')
+        end
+
+        def active?
+          active == true || (active == nil && index == 0)
         end
       end
     end
